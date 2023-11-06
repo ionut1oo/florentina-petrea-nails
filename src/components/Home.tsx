c5dfc3564f343d31e5ad7d02d25fd29068460c64
ionut ciornei <Pantelimon194@gmail.com>
2023-11-06 12:54:53 +0200

    Update .gitignore to include build folder

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -10,7 +10,6 @@ const Home = () => {
          <img className='d-block w-100 picture-slide' src='/imgs/img1.jpg' alt='first model' />
           <Carousel.Caption>
              <h5 className='text-slide mt-4'> <span className='span-slide me-2'> Florentina </span> <span className='span2-slide'> Petrea Nails </span>  </h5>
-             {/* <p className='paragraph-slide'> Unghii, unghiute sau modele, cu drag eu ma ocup de ele! </p> * */}
           </Carousel.Caption>
        </Carousel.Item>
        <Carousel.Item>
